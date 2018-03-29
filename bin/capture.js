09ae081e8456a816697655aabd9f8cc8792b3aaf
John Guidry <lowdowndirtyrottenscoundrel@gmail.com>
2018-03-28 23:25:35 -0500

    Add CLI options parsing

--- bin/capture.js
+++ bin/capture.js
@@ -1,5 +1,57 @@
 #!/usr/bin/env node
+const parseArgs = require('minimist')
 const highland = require('highland')
-const captureReadable = require('../')
+const CaptureReadableStream = require('../')
 
-highland(process.stdin).through(captureReadable).pipe(process.stdout)
+const DEFAULTS = {
+  'pool-size': 2,
+  'output': './'
+}
+
+const ALIASES = {
+  'output': 'o',
+  'verbose': 'v',
+  'help': 'h'
+}
+
+const argv = parseArgs(process.argv.slice(2), {
+  default: DEFAULTS,
+  alias: ALIASES,
+  boolean: ['verbose', 'help']
+})
+
+const usage = () =>
+  `Usage: capture-readable [<option>...] <URI>...
+
+Options:
+  --pool-size    maximum number of headless Chromium instances to allocate  [default: ${DEFAULTS['pool-size']}]
+  --output, -o   destination folder for captured HTML                       [default: ${DEFAULTS['output']}]
+  --verbose, -v  enable debugging output
+  --help, -h     display this message
+`
+
+const MISSING_ARGUMENTS = 'One or more URIs must be provided. See `capture-readable --help` for usage notes.'
+
+const fail = (msg) => {
+  console.error(msg)
+  process.exit(1)
+}
+
+if (argv['help']) {
+  console.log(usage())
+  process.exit()
+}
+
+const uris = process.stdin.isTTY
+  ? (argv._.length > 0) ? argv._ : fail(MISSING_ARGUMENTS)
+  : process.stdin
+
+const opts = {
+  poolSize: argv['pool-size'],
+  outputDirectory: argv['output'],
+  verbose: argv['verbose']
+}
+
+highland(uris)
+  .through(CaptureReadableStream.create(opts))
+  .pipe(process.stdout)
--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const path = require('path')
 const { URL } = require('url')
+const { pipeline, split, compact, through, map } = require('highland')
 const ReadabilityStream = require('./lib/readability-stream')
 const fileWriterStream = require('./lib/file-writer-stream')
 
-const captureReadable = (urlStream) =>
-  urlStream
-    .split()
-    .compact()
-    .through(ReadabilityStream.create())
-    .map(generateFileContent)
-    .through(fileWriterStream)
-    .map((p) => `${p}\n`)
+const prepareFileStream = ({ outputDirectory }) =>
+  ({ uri, title, content }) => {
+    let url = new URL(uri.spec, uri.prePath)
+    let dir = path.join(outputDirectory, url.host, encodeURIComponent(url.pathname.slice(1)))
+    let base = `${Date.now()}.readable.html`
+    let filename = path.format({ dir, base })
 
-function generateFileContent ({ uri, title, content }) {
-  let url = new URL(uri.spec, uri.prePath)
-  let dir = path.join(url.host, encodeURIComponent(url.pathname.slice(1)))
-  let base = `${Date.now()}.readable.html`
-  let filename = path.format({ dir, base })
-
-  return {
-    filename,
-    content: `${title}\n${content}`
+    return {
+      filename,
+      content: `${title}\n${content}`
+    }
   }
-}
 
-module.exports = captureReadable
+const create = (opts) =>
+  pipeline(
+    split(),
+    compact(),
+    through(ReadabilityStream.create(opts)),
+    map(prepareFileStream(opts)),
+    through(fileWriterStream),
+    map((p) => `${p}\n`)
+  )
+
+module.exports = { create }
